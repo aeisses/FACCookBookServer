7ebbb18729409c87ce42d9c73d8abcff1e73ebab
Aaron Eisses <aaron.eisses@gmail.com>
2015-03-23 22:24:13 -0300

    The basic of the server are implemented.

--- rest.js
+++ rest.js
@@ -1,11 +1,26 @@
+
+var fs = require('fs');
 var restify = require("restify");
 var server = restify.createServer();
- 
-function respond(req, res, next) {
-    res.send('Hello Restify!');
+
+function getrespond(req, res, next) {
+  res.send('Get: Recipe!');
+  return next();
 }
  
-server.get('/', respond);
+function updaterespond(req, res, next) {
+  res.send({Update:  req.params.date});
+  return next();
+}
+
+function getimage(req, res, next) {
+  var img = fs.readFileSync('./'+req.params.image);
+  res.writeHead( 200, {'Content-Type': 'image/png' });
+  res.send(img, 'binary');
+}
+
+server.get('/recipe', getrespond);
+server.get('/update/:date', updaterespond);
  
 var port = process.env.PORT || 5000;
 server.listen(port, function() {
